60b6cee1fd8050013f4b468ef79e0033b415be4a
Alexander <alexanderl96@yahoo.com>
2021-03-03 01:53:50 -0500

    2

--- Web-site.js
+++ Web-site.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    
+      let h1 = document.querySelector('h1')
+      h1.addEventListener("mouseover", (e)=>{
+          e.target.style.color= "green";
+          h1.addEventListener("mouseover", (e)=>{
+            e.target.style.color= "blue"; 
+        })
+      })
+     
+     
 })

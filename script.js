12794c0106821bc3f0ed22bea39f5f0eade2c71e
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-03-10 12:36:10 -0500

    Links working 20%

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Loading the DOM 
 
 document.addEventListener('DOMContentLoaded', (eve) => {
+    eve.preventDefault()
+
+let linkSnowman = document.getElementById('linkSnowman')
+linkSnowman.addEventListener('click',()=>{
+    let location = linkSnowman.getAttribute('href');
+    window.open(location, '_blank');
+})
+
+let linkTrader = document.getElementById('linkTrader')
+linkTrader.addEventListener('click',()=>{
+    let location = linkTrader.getAttribute('href');
+    window.open(location, '_blank');
+})
 
 })

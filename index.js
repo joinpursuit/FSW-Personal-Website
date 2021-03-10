519294a7aa09df30b14b94c4b9672e4688bab97d
Coreen Cooper <coreencooper@pursuit.org>
2021-03-10 15:08:16 -0500

    create createNav()

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// point button to next section
+
 const jumpToSection = (location) => {
     location.scrollIntoView(true);
 }
@@ -23,14 +25,32 @@ contactBtn.addEventListener("click", () => {
     jumpToSection(contactSection);
 })
 
-
-
-
-
-
-
 // aboutBtn.addEventListener("click", () => {
 //     aboutSection.scrollIntoView(true);
 // })
+
+
+// generate nav to each section
+// const homeSection = document.querySelector("#home-section");
+
+// const createNav = (section) => {
+//     const navLinks = ["Home", "About", "Project", "Contact"];
+//     const nav = document.createElement("nav");
+//     const ul = document.createElement("ul");
+//     section.appendChild(nav);
+//     nav.appendChild(ul);
+//     for (let i = 0; i < navLinks.length; i++) {
+//         const li = document.createElement("li");
+//         li.textContent = navLinks[i];
+//         ul.appendChild(li);
+//     }
+
+// }
+
+// const setLiAttribute = (id) => {
+//     li.setAttribute("href", id);
+// }
+
+
 
 

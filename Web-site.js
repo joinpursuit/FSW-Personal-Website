1070be069e6b840003dea2b4b36654fb8b89d91b
Alexander <alexanderl96@yahoo.com>
2021-03-11 03:28:17 -0500

    dfg

--- Web-site.js
+++ Web-site.js
@@ -1,40 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-      let h1 = document.querySelector('h1')
-      h1.addEventListener("mouseover", (e)=>{
-        if(e.target.style.color !== "green"){
-          e.target.style.color = "green"
-        }else{
-          e.target.style.color= "blue"
-        }
-      })
+  let h1 = document.querySelector('h1')
+  h1.addEventListener("mouseover", (e)=>{
+    if(e.target.style.color !== "green"){
+      e.target.style.color = "green"
+    }else{
+      e.target.style.color= "blue"
+    }
+  })
       
-      function change(){
-        let img = document.querySelector('#img');
-        if(img.src !== "https://i.imgur.com/8Ba9n5K.jpg"){
-         img.src = "https://i.imgur.com/8Ba9n5K.jpg"
-        }else{
-        img.src = "https://i.imgur.com/4qMZueg.jpg"
-       }
+
+  function change(){
+    let img = document.querySelector('#img');
+      if(img.src !== "https://i.imgur.com/8Ba9n5K.jpg"){
+        img.src = "https://i.imgur.com/8Ba9n5K.jpg"
+      }else{
+      img.src = "https://i.imgur.com/4qMZueg.jpg"
       }
-     img.addEventListener("mouseover", ()=>{change()})
-
-     let bullet = document.querySelector('.bullet')
-      bullet.addEventListener("click", (e)=>{
-        if(e.document.style.background !== "green"){
-          e.document.style.background = "green"
-        }else{
-          e.document.style.background = "red"
-        }
-      })
+  }
+  img.addEventListener("mouseover", ()=>{change()})
 
 
 
-      
-    
-
-      
-
-     
-
-      
+  let bullet = document.querySelector('.bullet')
+  bullet.addEventListener("click", (e)=>{
+    if(e.document.style.background !== "green"){
+      e.document.style.background = "green"
+    }else{
+      e.document.style.background = "red"
+    }
+  })
+  
 })

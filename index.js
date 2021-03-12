1e1c37e0d59578e7bcd31ff7895a9d9379107e17
Coreen Cooper <coreencooper@pursuit.org>
2021-03-12 11:45:36 -0500

    create displayNavbar() and restructure js file

--- index.js
+++ index.js
@@ -1,91 +1,77 @@
+const aboutSection = document.querySelector("#about-section");
+const projectsSection = document.querySelector("#projects-section");
+const contactSection = document.querySelector("#contact-section");
+
+const aboutBtn = document.querySelector("#about-btn");
+const projectBtn = document.querySelector("#project-btn");
+const contactBtn = document.querySelector("#contact-btn");
+
 // why doesn't page go back to the top when refreshed?
 
-// split name into array
+//// ***FUNCTIONS*** //////////////////////////////////////////
 
+// Create divs with letters for my name
 const displayLetters = () => {
-    const coreenInAnArray = ["C","o","r","e","e","n","","C","o","o","p","e","r"];
-    const header = document.querySelector("header");
-    for (let i = 0; i < coreenInAnArray.length; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("id", "letter" + i);
-        div.classList.add("letters");
-        div.textContent = coreenInAnArray[i];
-        header.appendChild(div);
-    }
+  const coreenInAnArray = [
+    "C",
+    "o",
+    "r",
+    "e",
+    "e",
+    "n",
+    "",
+    "C",
+    "o",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
+  const header = document.querySelector("header");
+  for (let i = 0; i < coreenInAnArray.length; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "letter" + i);
+    div.classList.add("letters");
+    div.textContent = coreenInAnArray[i];
+    header.appendChild(div);
+  }
   return header;
 };
 
-window.addEventListener("load", displayLetters);
+// create navbar
+const displayNavBar = () => {
+    const navItems = ["Home", "About", "Projects", "Contact"];
+    const navBar = document.querySelector("nav > ul");
+    for (let i = 0; i < navItems.length; i++) {
+      const li = document.createElement("li");
+      li.classList.add("nav-li");
+      li.textContent = navItems[i];
+      navBar.appendChild(li);
+    }
+    return navBar;
+  };
 
-// point button to next section
+// move to next section
 const jumpToSection = (location) => {
-  location.scrollIntoView(true);
+    location.scrollIntoView(true);
 };
 
-const aboutBtn = document.querySelector("#about-btn");
-const aboutSection = document.querySelector("#about-section");
 
+//// ***EVENT LISTENERS*** ////////////////////////////////////////////////////
+
+// display header and navbar on page load
+window.addEventListener("load", () => {
+    displayNavBar();
+    displayLetters();
+});
+
+// move to section and display navbar with button
 aboutBtn.addEventListener("click", () => {
   jumpToSection(aboutSection);
 });
-
-const projectBtn = document.querySelector("#project-btn");
-const projectsSection = document.querySelector("#projects-section");
-
 projectBtn.addEventListener("click", () => {
   jumpToSection(projectsSection);
 });
-
-const contactBtn = document.querySelector("#contact-btn");
-const contactSection = document.querySelector("#contact-section");
-
 contactBtn.addEventListener("click", () => {
   jumpToSection(contactSection);
 });
-
-// aboutBtn.addEventListener("click", () => {
-//     aboutSection.scrollIntoView(true);
-// })
-
-// generate nav to each section
-
-// const navLinks = 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const homeSection = document.querySelector("#home-section");
-
-// const createNav = (section) => {
-//     const navLinks = ["Home", "About", "Project", "Contact"];
-//     const nav = document.createElement("nav");
-//     const ul = document.createElement("ul");
-//     section.appendChild(nav);
-//     nav.appendChild(ul);
-//     for (let i = 0; i < navLinks.length; i++) {
-//         const li = document.createElement("li");
-//         li.textContent = navLinks[i];
-//         ul.appendChild(li);
-//     }
-
-// }
-
-// const setLiAttribute = (id) => {
-//     li.setAttribute("href", id);
-// }

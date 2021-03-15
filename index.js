b7116f741c55221f990f3222ff09ac886b2d144f
Coreen Cooper <coreencooper@pursuit.org>
2021-03-15 11:56:36 -0400

    attach navBar() to each section

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+"use strict"
+
+//////////////////// ***GLOBAL VARIABLES*** ///////////////////////////
+const navListItems = ["Home", "About", "Projects", "Contact"];
 const homeSection = document.querySelector("#home-section");
-const aboutSection = document.querySelector("#about-section");
-const projectsSection = document.querySelector("#projects-section");
-const contactSection = document.querySelector("#contact-section");
-
-const aboutBtn = document.querySelector("#about-btn");
-const projectBtn = document.querySelector("#project-btn");
-const contactBtn = document.querySelector("#contact-btn");
-
-// why doesn't page go back to the top when refreshed?
-// I saw innerHTML being used to create tags // EXAMPLE: section.innerHTML = "<span id="create">" + "hello" + "</span>" // which is industry standard?
-
-//// ***FUNCTIONS*** //////////////////////////////////////////
-
-// create divs with letters for my name
 const name = document.querySelector("#name");
 const coreenInAnArray = [
   "C",
@@ -29,6 +19,25 @@ const coreenInAnArray = [
   "e",
   "r",
 ];
+
+const aboutSection = document.querySelector("#about-section");
+const aboutBtn = document.querySelector("#about-btn");
+
+const projectsSection = document.querySelector("#projects-section");
+const projectBtn = document.querySelector("#project-btn");
+
+const contactSection = document.querySelector("#contact-section");
+const contactBtn = document.querySelector("#contact-btn");
+
+
+// why doesn't page go back to the top when refreshed? // location.reload() // 
+// want navbar to appear when user gets to the new section // is that CSS or js? //
+// is it better to have all the global variables at the top or next to the function? //
+// is it better to have the variable declared in the function it's being used in or global? //
+
+///////////////////////// ***FUNCTIONS*** ////////////////////////////////
+
+// create divs with letters for my name ///////////////////////////////
 const displayLetters = () => {
   coreenInAnArray.forEach((letter, i) => {
     const div = document.createElement("div");
@@ -39,8 +48,7 @@ const displayLetters = () => {
   });
 };
 
-const navListItems = ["Home", "About", "Projects", "Contact"];
-
+// create navBar //////////////////////////////////////////////////////////
 const createNavbar = (section) => {
   const nav = document.createElement("nav");
   const ul = document.createElement("ul");
@@ -54,71 +62,6 @@ const createNavbar = (section) => {
     li.appendChild(a);
   });
 };
-// // create navbar
-// // how could I make this cleaner?
-// // use map or object
-// const hrefValues = [
-//   "#home-section",
-//   "#about-section",
-//   "#projects-section",
-//   "#contact-section",
-// ];
-
-// // const hrefValue2 = {
-// //   homeSection: "#home-nav",
-// //   aboutSection: "#about-nav",
-// //   projectsSection: "#projects-nav",
-// //   contactSection: "#contact-nav",
-// // };
-
-// const tags = {
-//   home: {nav: "#home-nav", ul: "#home-ul"},
-//   about: {nav: "#about-nav", ul: "#about-ul"},
-//   projects: {nav: "#projects-nav", ul: "#projects-ul"},
-//   contact: {nav: "#contact-nav", ul: "#contact-ul"},
-// };
-// // const section = document.querySelectorAll("body > section");
-
-// // nav is created in the correct section
-// // but ul does not know where to append ***appending ul to home only***
-// // create id to grab nav in the correct section
-// // then repeat for ul
-
-// const createNav = (grabTag, key) => {
-//   const nav = document.createElement("nav");
-//   nav.setAttribute("id", tags[key]["nav"]);
-//   grabTag.prepend(nav);
-// };
-
-// const createUl = () => {
-
-//   // we invoke function
-// // if  (jumpToSection(aboutSection)) {
-// // } then nav = document.querySelector("nav")
-// // else {
-// // }const nav = document.querySelector("nav")
-
-//   const nav = document.querySelector("nav");
-//   // ***** the problem starts here *****////
-//   const ul = document.createElement("ul");
-//   nav.appendChild(ul);
-// };
-
-// const displayNavBar = () => {
-//   const ul = document.querySelector("ul");
-//   for (let i = 0; i < navItems.length; i++) {
-//     const li = document.createElement("li");
-//     const a = document.createElement("a");
-//     li.classList.add("nav-li");
-//     a.setAttribute("href", hrefValues[i]);
-//     li.textContent = a[i];
-//     a.textContent = navItems[i];
-//     ul.appendChild(li);
-//     li.appendChild(a);
-//   }
-//   debugger;
-//   return navItems;
-// };
 
 // move to next section ////////////////////////////////////////////////////////
 const jumpToSection = (section) => {
@@ -130,27 +73,20 @@ const jumpToSection = (section) => {
 // display header and navbar on page load
 window.addEventListener("load", () => {
   createNavbar(homeSection);
-  // createUl();
-  // displayNavBar();
   displayLetters();
 });
 
 // move to section and display navbar with button
 aboutBtn.addEventListener("click", () => {
-  // createNav(aboutSection, "about");
-  // createUl(aboutSection, ); // ul created
-  // displayNavBar();
+  createNavbar(aboutSection);
   jumpToSection(aboutSection);
 });
 projectBtn.addEventListener("click", () => {
-  // createUl();
-  // displayNavBar();
-  // createNav(projectsSection, "projects");
+  createNavbar(projectsSection);
   jumpToSection(projectsSection);
 });
 contactBtn.addEventListener("click", () => {
-  // createUl();
-  // displayNavBar();
-  // createNav(contactSection, "contact");
+  createNavbar(contactSection);
   jumpToSection(contactSection);
 });
+

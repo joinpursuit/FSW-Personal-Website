4885c08aee91505c6eb33e446b182f815ec09c0d
Kobe <tahiriadam@gmail.com>
2021-03-18 15:19:33 -0400

    added hamburger menu w JS for responsivness

---
+++ myWebsite.js
@@ -0,0 +1,23 @@
+const navBar = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-bar");
+    const navLinks = document.querySelectorAll(".nav-bar li");
+
+    burger.addEventListener("click", (e) => {
+        //Toggle the burger Nav
+        nav.classList.toggle("nav-active");
+        //Animation for Links in Nav 
+        navLinks.forEach((link, i) => {
+            if(link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade .5s ease forwards ${i / 6 + .3}s`
+            }
+        });
+        //Burger X animation
+        burger.classList.toggle("toggle");
+    })
+
+}
+
+navBar();

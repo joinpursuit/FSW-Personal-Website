5d66b4f8fb04e02f86ca38f13875d466e6ab012a
Alexander <alexanderl96@yahoo.com>
2021-03-18 02:53:15 -0400

    sfgafv

--- contact.js
+++ contact.js
@@ -1,26 +1,32 @@
 document.addEventListener('DOMContentLoaded', e =>{
     e.preventDefault()
     const name = document.getElementById('form1')
-    console.log(name)
     const lastName = document.getElementById('form2')
-    console.log(lastName)
-    
     const email = document.getElementById('form3')
-    console.log(email)
-    
     const text = document.getElementById('form4')
-    console.log(text)
-    
     const button= document.getElementById('form5') 
-    button.addEventListener('click', e =>{
+    button.addEventListener('submit', e =>{
         e.preventDefault()
         const norm = document.getElementById('norm')
         let info = `${name.value} ${lastName.value} \n ${email.value} \n ${text.value}`
-        console.log(info)
         norm.value= info
+        sendEmail(name.value, lastName.value, email.value, text.value)
         
-
     } )
+    
   
 })
 
+
+function sendEmail(name, lastName, email, text){
+    Email.send({
+        Host: "smpt.gmail.com",
+        Username: 'alexander.larosa.perez@gmail.com',
+        To: "alexander.larosa.perez@gmail.com",
+        From:  "alexander.larosa.perez@gmail.com",
+        Subject: `${name.value} sent you a msg`,
+        Password: "dkelqhtqsquwjfvj",
+        Body: `Name: ${name.value}  ${lastName.value}<br/> Email: ${email.value} 
+        <br/> Message: ${text.value}`
+    }).then((message)=> alert('mail send successfully'))
+}

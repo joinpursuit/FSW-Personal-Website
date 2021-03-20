eacc5c125cca4f95a02d0b55dcb406f1f775c15c
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-03-20 14:47:21 -0400

    Added tabs and info

---
+++ main.js
@@ -0,0 +1,10 @@
+document.onreadystatechange = function() {
+    var state = document.readyState
+    if (state == 'complete') {
+      setTimeout(function(){
+        document.getElementById('interactive');
+  
+  document.getElementById('load').style.visibility="hidden";
+  },4000);
+    }
+  }

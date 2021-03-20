d0cf30b7950b02958170b7e03b9605017cabba5c
joshuamendez <joshuafmendez@gmail.com>
2021-03-20 14:38:44 -0400

    started new and now working on test file"

---
+++ index.js
@@ -0,0 +1,36 @@
+const smallMenu = () => {
+    let x = document.querySelector("#navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//         for (i = 0; i < dots.length; i++) {
+//             dots[i].className = dots[i].className.replace(" active", "");
+//         }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+// code up a functioning form
+// expanded details about project
+// project picture at expand out readme file a link to github
+
+// possible implementaion of an API
+// constants overall activity for commits to github for display
+// on projects page

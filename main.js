01a847161921860230e26aca00cc8b854a488bb6
Obaid Rustemi <orustemi@gmail.com>
2021-03-20 14:43:17 -0400

    adding projects page

---
+++ main.js
@@ -0,0 +1,10 @@
+const overlayLinks = document.querySelector(".overlay-links");
+
+overlayLinks.addEventListener("click", (e) => {
+  const menuCheckbox = (document.querySelector(".toggler").checked = false);
+});
+
+$(document).ready(function(){
+    $('html').animate({scrollTop:0}, 1);
+    $('body').animate({scrollTop:0}, 1);
+});

147ef5f71a1c500fbc1b67787be43847b6064c99
Jordan Bobadilla <jordanboba08@gmail.com>
2021-03-20 14:40:13 -0400

    responsiveness on about page

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const button = document.querySelector("button")
-button.addEventListener("click", () => {
+const welcomeButton = document.querySelector(".btn")
+welcomeButton.addEventListener("click", () => {
     document.location.href = "./about.html" 
+})
+
+const changeButton = document.querySelector(".change")
+changeButton.addEventListener("click", () => {
+    const header = document.querySelector("header")
+    const nav = document.querySelector("nav")
+
+    header.style.color = "#0b0b0b"
+    nav.style.color = "white"
+    console.log("hello");
 })

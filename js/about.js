ea56b922efedb827dbd56c2fc3c809a8b9a74558
Jordan Bobadilla <jordanboba08@gmail.com>
2021-03-21 16:40:21 -0400

    skills done

--- js/about.js
+++ js/about.js
@@ -1,11 +1,13 @@
 const checkbox = document.querySelector('input[name=theme]');
 const header = document.querySelector("header")
 const main = document.querySelector("main")
-checkbox.addEventListener('change', () => {
+
+checkbox.addEventListener('click', (e) => {
     if(checkbox.checked) {
         transition()
         header.setAttribute('data-theme', 'dark')
         main.setAttribute('data-theme', 'dark')
+        localStorage.setItem("changed", "enabled")
     } else {
         transition()
         header.setAttribute('data-theme', 'light')
@@ -14,15 +16,14 @@ checkbox.addEventListener('change', () => {
 })
 
 let transition = () => {
-    header.classList.add('transition');
     window.setTimeout(() => {
-        header.classList.remove('transition')
+        header.classList.toggle('transition')
     }, 1000)
 }
 
 const menu_btn = document.querySelector('.fa-bars');
 const mobile_menu = document.querySelector('.mobile-nav');
 
-	menu_btn.addEventListener('click', () => {
-		mobile_menu.classList.toggle('is-active');
-	});
+menu_btn.addEventListener('click', () => {
+	mobile_menu.classList.toggle('is-active');
+});

5d8f6e60bc1fc2e13bdeb6d658b732e7b0db7496
Durdona Djalilova <ddjalilova3@gmail.com>
2021-03-21 16:56:40 -0400

    almost there

--- my-website.js
+++ my-website.js
@@ -6,6 +6,11 @@ const adventureVideo = document.querySelector("#adventure-video")
 const snowmanVideo = document.querySelector("#snowman-video")
 const clickHereSnowman = document.querySelector("#click-here-snowman")
 const clickHereAdventure = document.querySelector("#click-here-adventure")
+const descriptionAdventure = document.querySelector("#descriptionAdventure")
+const descriptionSnowman = document.querySelector("#descriptionSnowman")
+const buttonSnowman = document.querySelector("#btn-snowman")
+const buttonAdventure = document.querySelector("#btn-adventure")
+
 
 
 clickHereSnowman.addEventListener("click", () => {
@@ -30,12 +35,28 @@ clickHereAdventure.addEventListener("click", () => {
  })
 
 
-//  button.addEventListener("click", () => {
-//    setTimeout( () => {
-//      window.open("mailto:" + document.getElementById('email').value + "?subject=" + document.getElementById('subject').value + "&body=" + document.getElementById('message').value);
-//    }, 320);
-//  }) 
-function send() {
+ buttonSnowman.addEventListener("click", () => {
+   if(descriptionSnowman.style.display === "none") {
+     descriptionSnowman.style.display = "block"
+     buttonSnowman.textContent = `Show less`
+    } else {
+     buttonSnowman.textContent = `Show description`
+     descriptionSnowman.style.display = "none"
+
+   }
+ }) 
+
+ buttonAdventure.addEventListener("click", () => {
+  if(descriptionAdventure.style.display === "none") {
+    descriptionAdventure.style.display = "block"
+    buttonAdventure.textContent = `Show less`
+   } else {
+    buttonAdventure.textContent = `Show description`
+    descriptionAdventure.style.display = "none"
+
+  }
+}) 
+ function send() {
   setTimeout(function() {
     window.open("mailto:" + document.getElementById('email').value + "?subject=" + document.getElementById('subject').value + "&body=" + document.getElementById('message').value);
   }, 320);

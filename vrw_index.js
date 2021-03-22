731ce68f5c9377fe80c5160bbb78c2210033671e
vrwatson <vanessawatson@pursuit.org>
2021-03-22 03:46:33 -0400

    formatting index & adding linked icons

---
+++ vrw_index.js
@@ -0,0 +1,30 @@
+const projectLink = document.querySelector("#project-link");
+
+projectLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+projectLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+const abtMeLink = document.querySelector("#abtMe-link");
+
+abtMeLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+abtMeLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+
+const cntctLink = document.querySelector("#cntct-link");
+
+cntctLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+cntctLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
---
+++ vrw_project.js
@@ -0,0 +1,52 @@
+const jokesRepo = document.querySelector("#jokes-github-link");
+
+jokesRepo.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = "#8bbbae";
+})
+
+jokesRepo.addEventListener("mouseleave", (e) => {
+    e.target.style.backgroundColor = "#ECECFA";
+})
+
+const diceRepo = document.querySelector("#dice-github-link");
+
+diceRepo.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = "#8bbbae";
+})
+
+diceRepo.addEventListener("mouseleave", (e) => {
+    e.target.style.backgroundColor = "#ECECFA";
+})
+
+const homeLink = document.querySelector("#home-link");
+
+homeLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+homeLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+const abtMeLink = document.querySelector("#aboutMe-link");
+
+abtMeLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+abtMeLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+
+const cntctLink = document.querySelector("#contact-link");
+
+cntctLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+cntctLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+

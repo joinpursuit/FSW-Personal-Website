9c60f911f87b901b034fb862b21a9bba5af9bce3
Teraesa  Metivier <teraesam@yahoo.com>
2021-03-21 22:24:52 -0400

    working on project

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ new URLSearchParams(window.location.search).forEach((name1, name2) => {
   resultsList.append(`${name1}: ${name2}:`);
   resultsList.append(document.createElement("br"));
 });
+
+const div = document.getElementById("text-based-game");
+const textGameImage = document.querySelector("#text-game");
+const button = document.querySelector("button");
+const p = document.getElementById("intro");
+div.addEventListener("click", (event) => {
+  if (p.style.display === "none") {
+    p.style.display = "block";
+  } else {
+    p.style.display = "none";
+  }
+});

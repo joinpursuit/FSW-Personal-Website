ed0a55a8c6a96a54022bdc17d6a5883aa45ea6cf
Durdona Djalilova <ddjalilova3@gmail.com>
2021-03-21 23:48:32 -0400

    finish

--- my-website.js
+++ my-website.js
@@ -1,63 +1,42 @@
-const aboutMe = document.querySelector("about-me-section")
-const aboutMeHeader = document.querySelector("#about-me-header")
-const projects = document.querySelector("#projects-section")
-const projectsHeader = document.querySelector("#projects-header")
-const adventureVideo = document.querySelector("#adventure-video")
-const snowmanVideo = document.querySelector("#snowman-video")
-const clickHereSnowman = document.querySelector("#click-here-snowman")
-const clickHereAdventure = document.querySelector("#click-here-adventure")
-const descriptionAdventure = document.querySelector("#descriptionAdventure")
-const descriptionSnowman = document.querySelector("#descriptionSnowman")
-const buttonSnowman = document.querySelector("#btn-snowman")
-const buttonAdventure = document.querySelector("#btn-adventure")
-
-
+const projects = document.querySelector("#projects-section");
+const projectsHeader = document.querySelector("#projects-header");
+const adventureVideo = document.querySelector("#adventure-video");
+const snowmanVideo = document.querySelector("#snowman-video");
+const clickHereSnowman = document.querySelector("#click-here-snowman");
+const clickHereAdventure = document.querySelector("#click-here-adventure");
+const descriptionAdventure = document.querySelector("#descriptionAdventure");
+const descriptionSnowman = document.querySelector("#descriptionSnowman");
+const buttonSnowman = document.querySelector("#btn-snowman");
+const buttonAdventure = document.querySelector("#btn-adventure");
 
 clickHereSnowman.addEventListener("click", () => {
   clickHereSnowman.style.display = "none";
-    snowmanVideo.play();
-    clickHereSnowman.style.display = "inline block";
-    // snowmanVideo.pause();
-    snowmanVideo.currentTime = 0;
-    // snowmanVideo.style.classList.add("blurEffect");
- })
+  snowmanVideo.play();
+  clickHereSnowman.style.display = "inline block";
+});
 
-// snowmanVideo.addEventListener("click", (e) => {
-//   e.target.play()
-//  })
 clickHereAdventure.addEventListener("click", () => {
   clickHereAdventure.style.display = "none";
-    adventureVideo.play();
-    clickHereAdventure.style.display = "inline block";
-    // snowmanVideo.pause();
-    snowmanVideo.currentTime = 0;
-    // snowmanVideo.style.classList.add("blurEffect");
- })
+  adventureVideo.play();
+  clickHereAdventure.style.display = "inline block";
+});
 
-
- buttonSnowman.addEventListener("click", () => {
-   if(descriptionSnowman.style.display === "none") {
-     descriptionSnowman.style.display = "block"
-     buttonSnowman.textContent = `Show less`
-    } else {
-     buttonSnowman.textContent = `Show description`
-     descriptionSnowman.style.display = "none"
-
-   }
- }) 
-
- buttonAdventure.addEventListener("click", () => {
-  if(descriptionAdventure.style.display === "none") {
-    descriptionAdventure.style.display = "block"
-    buttonAdventure.textContent = `Show less`
-   } else {
-    buttonAdventure.textContent = `Show description`
-    descriptionAdventure.style.display = "none"
-
+buttonSnowman.addEventListener("click", () => {
+  if (descriptionSnowman.style.display === "none") {
+    descriptionSnowman.style.display = "block";
+    buttonSnowman.textContent = `Show less`;
+  } else {
+    buttonSnowman.textContent = `Show description`;
+    descriptionSnowman.style.display = "none";
   }
-}) 
- function send() {
-  setTimeout(function() {
-    window.open("mailto:" + document.getElementById('email').value + "?subject=" + document.getElementById('subject').value + "&body=" + document.getElementById('message').value);
-  }, 320);
-}
+});
+
+buttonAdventure.addEventListener("click", () => {
+  if (descriptionAdventure.style.display === "none") {
+    descriptionAdventure.style.display = "block";
+    buttonAdventure.textContent = `Show less`;
+  } else {
+    buttonAdventure.textContent = `Show description`;
+    descriptionAdventure.style.display = "none";
+  }
+});

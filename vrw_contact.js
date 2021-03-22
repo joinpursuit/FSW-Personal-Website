689a16d074dd084fefdcd76b5a9b23efcd7a4fa8
vrwatson <vanessawatson@pursuit.org>
2021-03-22 07:07:15 -0400

    contact page & formatting

--- vrw_contact.js
+++ vrw_contact.js
@@ -8,6 +8,17 @@ homeLink.addEventListener("mouseleave", (e) => {
     e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
 })
 
+const abtMeLink = document.querySelector("#aboutMe-link");
+
+abtMeLink.addEventListener("mouseenter", (e) => {
+    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
+})
+
+abtMeLink.addEventListener("mouseleave", (e) => {
+    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
+})
+
+
 const projectLink = document.querySelector("#pro-link");
 
 projectLink.addEventListener("mouseenter", (e) => {
@@ -17,16 +28,3 @@ projectLink.addEventListener("mouseenter", (e) => {
 projectLink.addEventListener("mouseleave", (e) => {
     e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
 })
-
-
-const cntctLink = document.querySelector("#contact-link");
-
-cntctLink.addEventListener("mouseenter", (e) => {
-    e.target.style.textShadow = "1px 1px 2px #8bbbae, 0 0 25px #2d213f, 0 0 5px #8bbbae";
-})
-
-cntctLink.addEventListener("mouseleave", (e) => {
-    e.target.style.textShadow = "0px 0px 0px #ECECFA, 0 0 0px #ECECFA, 0 0 0px #ECECFA";
-})
-
-

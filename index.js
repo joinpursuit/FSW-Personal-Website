0c974bd9845865d6387cbbe0ba4a42e66255e0b1
joshuamendez <joshuafmendez@gmail.com>
2021-03-21 20:49:32 -0400

    fixed bug with toggle switch

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// localStorage.clear();
 const smallMenu = () => {
     let x = document.querySelector("#navLinks");
     if (x.style.display === "block") {
@@ -9,38 +10,22 @@ const smallMenu = () => {
 
 const toggleSwitch = document.querySelector('.toggle-switch');
 const currentTheme = localStorage.getItem('theme');
+const currentToggle = localStorage.getItem('toggle');
 if (currentTheme === "dark") {
   document.body.classList.add("dark-theme");
+  toggleSwitch.checked = true;
 }
 toggleSwitch.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
-
   let theme = "light";
+  let toggle = 'unchecked';
   if (document.body.classList.contains("dark-theme")) {
     theme = "dark";
+    toggle = 'checked';
   }
   localStorage.setItem("theme", theme);
+  localStorage.setItem("toggle", toggle);
 });
-
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";  
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1}    
-//         for (i = 0; i < dots.length; i++) {
-//             dots[i].className = dots[i].className.replace(" active", "");
-//         }
-//     slides[slideIndex-1].style.display = "block";  
-//     dots[slideIndex-1].className += " active";
-//     setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
 
 // code up a functioning form
 // expanded details about project

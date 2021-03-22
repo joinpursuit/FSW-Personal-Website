78302361e1cbea795864e9ff1a9ff6b2b92a06cd
Teraesa  Metivier <teraesam@yahoo.com>
2021-03-21 23:47:54 -0400

    made changed to projects tab

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ new URLSearchParams(window.location.search).forEach((name1, name2) => {
   resultsList.append(document.createElement("br"));
 });
 
+//text based game
 const div = document.getElementById("text-based-game");
 const textGameImage = document.querySelector("#text-game");
 const button = document.querySelector("button");
@@ -14,5 +15,18 @@ div.addEventListener("click", (event) => {
     p.style.display = "block";
   } else {
     p.style.display = "none";
+  }
+});
+
+//snowman game
+const game = document.getElementById("snowman-game");
+const snowmanGameImage = document.querySelector("#snowman");
+const btn = document.querySelector("words-btn");
+const ptag = document.getElementById("introduction");
+game.addEventListener("click", (event) => {
+  if (ptag.style.display === "none") {
+    ptag.style.display = "block";
+  } else {
+    ptag.style.display = "none";
   }
 });

5d504505d7eb029b4017dce82c411dd9f81e1abb
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-03-22 20:48:18 -0400

    Used Corey's Feedback to improve

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-document.onreadystatechange = function() {
-    var state = document.readyState
-    if (state == 'complete') {
-      setTimeout(function(){
-        document.getElementById('interactive');
-  
-  document.getElementById('load').style.visibility="hidden";
-  },4000);
-    }
-  }
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  if (state == "complete") {
+    setTimeout(function () {
+      document.querySelector("#interactive");
 
-  const hidePhoto = () =>{
-      const ul = document.querySelector(".photography-set")
-      const li = document.querySelector(".photography-flex")
-      if(ul.style.visibility === "visible"){
-          ul.style.visibility = "hidden"
-      }else{
-          ul.style.visibility = "visible"
-      }
+      document.querySelector("#load").style.visibility = "hidden";
+    }, 4000);
   }
+};
+
+const hidePhoto = () => {
+  const ul = document.querySelector(".photography-set");
+  const li = document.querySelector(".photography-flex");
+  if (ul.style.visibility === "visible") {
+    ul.style.visibility = "hidden";
+  } else {
+    ul.style.visibility = "visible";
+  }
+};
+document.querySelector("#hidepics").addEventListener("click", hidePhoto);

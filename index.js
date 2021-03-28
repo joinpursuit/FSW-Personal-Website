311522ac980e42a3218336fcabb74d241a2b0215
Yashiry Azize <azizeyashiry@gmail.com>
2021-03-28 15:14:07 -0400

    updated errors

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-window.onscroll = function () {
-  stickyNavbar();
-};
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
-const stickyNavbar = () => {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
 
 const randQButton = document.querySelector("#random-q-button");
 const question = document.querySelector("#question");

ea996dbf8aaf0ad10ddb34ce02fbfa9b511c05a0
Marsha Hall <mphall77@gmail.com>
2021-11-09 09:54:34 -0500

    iris main page

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
-// toggle menu bar
-const toggleMenu = (menu) => {
-	menu.classList.toggle("change");
+// navBar effects on scroll
+window.addEventListener("scroll", function () {
+	const header = this.document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// active nav link focus
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+	section.forEach((sec) => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute("id");
+		if (top > offset && top < offset + height) {
+			navLinks.forEach((link) => {
+				link.classList.remove("active");
+				document
+					.querySelector("header nav a[href*=" + id + "]")
+					.classList.add("active");
+			});
+		}
+	});
 };
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-	document.getElementById("mySidenav").style.display = "block";
-	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+// display mobile menu
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector("nav");
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-	document.getElementById("mySidenav").style.display = "none";
-	document.body.style.backgroundColor = "#b2b2b2";
-}
+const mobileMenu = () => {
+	menu.classList.toggle("is-active");
+	menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// scroll button
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", () => {
+	scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+// scroll button function for back to top
+scrollBtn.addEventListener("click", () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
+
+// read more button
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".about-para-text");
+
+readMoreBtn.addEventListener("click", (e) => {
+	text.classList.toggle("show-more");
+	readMoreBtn.innerText === "READ MORE"
+		? (readMoreBtn.innerText = "READ LESS")
+		: (readMoreBtn.innerText = "READ MORE");
+});
